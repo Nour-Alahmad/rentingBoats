72dacb3a95dcdf9a3de3985a0df7e31318e15dd0
FadiBarsham <fadi.barsham.it@gmail.com>
2025-09-30 15:12:39 +0200

    new update

--- src/main/java/com/capgemini/Application.java
+++ src/main/java/com/capgemini/Application.java
@@ -3,6 +3,6 @@ package com.capgemini;
 public class Application {
 
     public static void main(String[] args) {
-        System.out.println("Hello World! Hiiiiiiiif");
+        System.out.println("Hello World! HiiiiiFromFadi");
     }
 }
