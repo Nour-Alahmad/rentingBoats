2271e3c91173a059666c8d7e1f7f0aec79ee4854
FadiBarsham <fadi.barsham.it@gmail.com>
2025-10-14 10:35:38 +0200

    f

---
+++ src/main/java/com/capgemini/Boats.java
@@ -0,0 +1,179 @@
+package com.capgemini;
+
+public class Boats{
+
+    private int id;
+    private String type;
+    private int capacity;
+    private double pricePerHalfHour;
+    private boolean isAvailable;
+
+    public void Rowing1(){
+        this.id = 1;
+        this.type = "Rowing";
+        this.capacity = 4;
+        this.pricePerHalfHour = 6;
+        this.isAvailable = true;
+        System.out.println("ID: "+ this.id + "| Type: " + this.type + "| Capacity: "+ this.capacity +
+        "| Price per half hour: "+ pricePerHalfHour + "." );
+    }
+
+    public void Rowing2(){
+        this.id = 2;
+        this.type = "Rowing";
+        this.capacity = 4;
+        this.pricePerHalfHour = 6;
+        this.isAvailable = true;
+    }
+
+    public void Kayak1(){
+        this.id = 3;
+        this.type = "Kayak";
+        this.capacity = 2;
+        this.pricePerHalfHour = 5;
+        this.isAvailable = true;
+    }
+
+    public void Kayak2(){
+        this.id = 4;
+        this.type = "Kayak";
+        this.capacity = 2;
+        this.pricePerHalfHour = 5;
+        this.isAvailable = true;
+    }
+
+    public void Kayak3(){
+        this.id = 5;
+        this.type = "Kayak";
+        this.capacity = 2;
+        this.pricePerHalfHour = 5;
+        this.isAvailable = true; 
+    }
+
+    public void Kayak4(){
+        this.id = 6;
+        this.type = "Kayak";
+        this.capacity = 2;
+        this.pricePerHalfHour = 5;
+        this.isAvailable = true;
+    }
+
+    public void Kayak5(){
+        this.id = 7;
+        this.type = "Kayak";
+        this.capacity = 2;
+        this.pricePerHalfHour = 5;
+        this.isAvailable = true;
+    }
+
+    public void Kayak6(){
+        this.id = 8;
+        this.type = "Kayak";
+        this.capacity = 1;
+        this.pricePerHalfHour = 4;
+        this.isAvailable = true;
+    }
+
+    public void Kayak7(){
+        this.id = 9;
+        this.type = "Kayak";
+        this.capacity = 1;
+        this.pricePerHalfHour = 4;
+        this.isAvailable = true;
+    }
+
+    public void Kayak8(){
+        this.id = 9;
+        this.type = "Kayak";
+        this.capacity = 1;
+        this.pricePerHalfHour = 4;
+        this.isAvailable = true;
+    }
+
+    public void Kayak9(){
+        this.id = 10;
+        this.type = "Kayak";
+        this.capacity = 1;
+        this.pricePerHalfHour = 4;
+        this.isAvailable = true;
+    }
+
+    public void Kayak10(){
+        this.id = 11;
+        this.type = "Kayak";
+        this.capacity = 1;
+        this.pricePerHalfHour = 4;
+        this.isAvailable = true;
+    }
+    public void Subs1(){
+        this.id = 12;
+        this.type = "Kayak";
+        this.capacity = 1;
+        this.pricePerHalfHour = 3;
+        this.isAvailable = true;
+    }
+    public void Subs2(){
+        this.id = 13;
+        this.type = "Kayak";
+        this.capacity = 1;
+        this.pricePerHalfHour = 3;
+        this.isAvailable = true;
+    }
+    public void Subs3(){
+        this.id = 13;
+        this.type = "Kayak";
+        this.capacity = 1;
+        this.pricePerHalfHour = 3;
+        this.isAvailable = true;
+    }
+    public void Subs4(){
+        this.id = 14;
+        this.type = "Kayak";
+        this.capacity = 1;
+        this.pricePerHalfHour = 3;
+        this.isAvailable = true;
+    }
+    public void Subs5(){
+        this.id = 15;
+        this.type = "Kayak";
+        this.capacity = 1;
+        this.pricePerHalfHour = 3;
+        this.isAvailable = true;
+    }
+    public void Subs6(){
+        this.id = 16;
+        this.type = "Kayak";
+        this.capacity = 1;
+        this.pricePerHalfHour = 3;
+        this.isAvailable = true;
+    }
+    public void Subs7(){
+        this.id = 17;
+        this.type = "Kayak";
+        this.capacity = 1;
+        this.pricePerHalfHour = 3;
+        this.isAvailable = true;
+    }
+    public void Subs8(){
+        this.id = 18;
+        this.type = "Kayak";
+        this.capacity = 1;
+        this.pricePerHalfHour = 3;
+        this.isAvailable = true;
+    }
+    public void Subs9(){
+        this.id = 19;
+        this.type = "Kayak";
+        this.capacity = 1;
+        this.pricePerHalfHour = 3;
+        this.isAvailable = true;
+    }
+    public void Subs10(){
+        this.id = 20;
+        this.type = "Kayak";
+        this.capacity = 1;
+        this.pricePerHalfHour = 3;
+        this.isAvailable = true;
+    }
+
+}
